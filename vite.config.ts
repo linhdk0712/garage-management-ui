a6bcf79c5991b5de32955f656f4e9889b17e471a
Đỗ Khánh Linh <linhdk1@shb.com.vn>
2025-06-11 22:36:18 +0700

    Configures environment variables for API URL
    Updates the application to utilize environment variables for managing the API base URL.
    This allows for different API endpoints to be configured for different environments (development, production).
    
    Adds build scripts for different environments (dev, prod).
    Adds a vercel configuration file.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
+export default defineConfig(({mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react(), tailwindcss()],
+    define: {
+      // Expose env variables to your app
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+    // Environment-specific configuration
+    server: {
+      port: 3000,
+      host: true,
+    },
+    build: {
+      outDir: 'dist',
+      sourcemap: mode === 'development',
+    },
+    envPrefix: 'VITE_',
+    base: '/'
+    
+  }
 })
